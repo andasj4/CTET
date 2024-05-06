211c1dedbeb9ce0804b58d8a25e1d24d641d1b81
Anders Sjödahl <anders.sjodahl@omegapoint.se>
2024-05-06 22:10:31 +0200

    Added ListProducts

--- ProductDataSolution/ProductDataFunction/ListProducts.cs
+++ ProductDataSolution/ProductDataFunction/ListProducts.cs
@@ -7,6 +7,10 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using ProductDataFunction.Models;
+using Microsoft.Azure.Cosmos.Table;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
 
 namespace ProductDataFunction
 {
@@ -14,9 +18,55 @@ namespace ProductDataFunction
     {
         [FunctionName("ListProducts")]
         public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req, ILogger log)
-        {            
+        {
+            var builder = new ConfigurationBuilder()
+                       .SetBasePath(Environment.CurrentDirectory)
+                       .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                       .AddEnvironmentVariables();
+            var configuration = builder.Build();
 
-            return new OkObjectResult(null);
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var incomingProduct = JsonConvert.DeserializeObject<Product>(requestBody);
+            var storageAccount = CloudStorageAccount.Parse(configuration["StorageConnectionString"]);
+
+            var products = GetProducts(storageAccount, log);
+
+            return new OkObjectResult(products);
+        }
+
+        private static ProductModel GetProducts(CloudStorageAccount storageAccount, ILogger log)
+        {
+            ProductModel products = new ProductModel { Products = new List<Product>() };
+
+            try
+            {
+                var tableClient = storageAccount.CreateCloudTableClient();
+                var table = tableClient.GetTableReference("Products");
+
+                var query = new TableQuery<ProductEntity>();
+                var entities = table.ExecuteQuery(query);
+                
+                foreach(var entity in entities)
+                {
+                    Product product = new Product
+                    {
+                        ProductNo = entity.ProductNo,
+                        ProductName = entity.ProductName,
+                        Price = entity.Price,
+                        Description = entity.Description,
+                        ModifiedDate = entity.LastUpdated
+                    };
+
+                    products.Products.Add(product);
+                }
+
+                return products;
+            }
+            catch (Exception ex)
+            {
+                log.LogError($"Failed to list products: {ex.InnerException}");
+                return null;
+            }
         }
     }
 }
